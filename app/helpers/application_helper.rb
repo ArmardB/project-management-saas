4bec564f81bfa2a9c310273bbefe7a468fc15914
armardb <armardbellamy@gmail.com>
2016-06-26 13:08:29 +0000

    Completed artifacts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
         class: "alert fade in alert-#{type} #{tag_class}"
       }.merge(options)
 
-      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", 
+      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close",
         "data-dismiss" => "alert")
 
       Array(message).each do |msg|
@@ -33,16 +33,16 @@ module ApplicationHelper
     Tenant.find(tenant_id).name
   end
 
-  def s3_link(tenant_id, artifact_key)
-    link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
-  end
-
   def class_name_for_tenant_form(tenant)
     return "cc_form" if tenant.payment.blank?
     ""
   end
-  
+
   def tenant_name(tenant_id)
     Tenant.find(tenant_id).name
+  end
+  
+  def s3_link(tenant_id, artifact_key)
+    link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
   end
 end
